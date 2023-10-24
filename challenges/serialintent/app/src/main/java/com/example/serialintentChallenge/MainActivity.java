2d79fb254a3bb391ea2b0afee913829c25a33ab1
mhetacc <101409463+mhetacc@users.noreply.github.com>
2023-10-24 17:52:00 +0200

    trying to get FlagContainer.getFlag()

--- challenges/serialintent/app/src/main/java/com/example/serialintentChallenge/MainActivity.java
+++ challenges/serialintent/app/src/main/java/com/example/serialintentChallenge/MainActivity.java
@@ -12,8 +12,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+//import com.example.victimapp.FlagContainer; //need to implement module in gradle
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MOBIOTSEC";
+
+    //problem: object are com.example.maliciousactivity.FlagContainer
+    protected class FlagContainer implements Serializable {
+        private String[] parts;
+        private ArrayList<Integer> perm;
+
+        public FlagContainer(String[] parts, ArrayList<Integer> perm) {
+            this.parts = parts;
+            this.perm = perm;
+        }
+
+        private String getFlag() {
+            int n = parts.length;
+            int i;
+            String b64 = new String();
+            for (i=0; i<n; i++) {
+                b64 += parts[perm.get(i)];
+            }
+
+            byte[] flagBytes = Base64.decode(b64, Base64.DEFAULT);
+            String flag = new String(flagBytes, Charset.defaultCharset());
+
+            return flag;
+        }
+    }
 
     ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -24,12 +57,19 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = result.getData();
                     Log.d(TAG, "SerialActivityResult = " + result.getData());
                     try {
-                        //try to get all keys of bundle
+
+                        //TODO error "ERROR Parcelable encountered ClassNotFoundException reading a Serializable object (name = com.example.victimapp.FlagContainer)"
+                        //possibile fix: shared user ID
+                        //or: java reflection (analize java class from within itself)
+                        //maybe modify object name
+                        //use shared library (unlikely)
+
+
+
                         Bundle extras = intent.getExtras();
-                        for (String key : extras.keySet()) { //see akk the keys
-                            Object value = extras.get(key);
-                            Log.d(TAG, "Part: " + result.getResultCode() + " - Key: " + key + " - value :" + value.toString());
-                        }
+                        extras.setClassLoader(FlagContainer.class.getClassLoader());
+                        FlagContainer flagContainer = (FlagContainer) extras.getSerializable("flag");
+                        String flag = flagContainer.getFlag();
                     } catch (NullPointerException e) {
                         Log.d(TAG, "ERROR result Extras NullPointerException " + e);
                     } catch (Exception e) {
